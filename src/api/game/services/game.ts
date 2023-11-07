9e26f5ba86701b9416a9f2e9f4e2ae2f0bf4cd36
Marcos Renê <renemoura789@gmail.com>
2023-11-07 16:31:00 -0300

    feat: fetch game data from GoG

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -1,15 +1,17 @@
 /**
  * game service
  */
-
+import axios from "axios";
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreService("api::game.game", () => ({
   async populate(params) {
-    const category = await strapi.service("api::category.category").find({
-      filters: { name: params.category },
-    });
+    const {
+      data: { products },
+    } = await axios.get(
+      `https://www.gog.com/games/ajax/filtered?mediaType=game?sort=rating`
+    );
 
-    console.log({ category });
+    console.log({ products });
   },
 }));
